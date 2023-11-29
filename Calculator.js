bb9cfeff2ca0fa4751663209914d757e34d2bc74
Prem_Saragada <149216070+PremReddy-585@users.noreply.github.com>
2023-11-29 20:28:59 +0530

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,2 +1,26 @@
-let calculatorElement = ("calculatorNumbers");
-console.log("calculatorElement");
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialchars = ["%", "*", "/", "-", "+", "="];
+let output = "";
+
+
+
+const calculate = (btnvalue) => {
+    if (btnvalue === "=" && output !== "") {
+        output = eval(output.replace("%", "/100"));
+    }
+    else if (btnvalue === "AC") {
+        output = ""
+    }
+    else if (btnvalue === "DEL") {
+        output = output.toString().slice(0, -1);
+    }else{
+        if(output === "" && specialchars.includes(btnvalue)) return;
+        output += btnvalue;
+    }
+
+    display.value = output;
+};
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
